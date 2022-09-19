b81a00d860aef6b50ba9a6454a73d2a62b83b2a9
snowy <93100014+sn0wyig@users.noreply.github.com>
2022-09-19 22:34:12 +0100

    3 Bottom pins now are used to send 3 automated phrases. Button A will stop timer now.

--- main.ts
+++ main.ts
@@ -1,21 +1,38 @@
 let elapsed = 0
 let stopwatch_start = 0
+let hello = 0
 let degrees = 0
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
+input.onPinPressed(TouchPin.P0, function () {
+    radio.sendNumber(0)
     music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-    radio.sendString("love")
-    basic.showString("test")
+    basic.showString("HI")
 })
-input.onButtonPressed(Button.AB, function () {
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("HI")
+})
+input.onButtonPressed(Button.A, function () {
     elapsed = input.runningTime() - stopwatch_start
     basic.showNumber(Math.idiv(elapsed, 1000))
 })
+input.onPinPressed(TouchPin.P2, function () {
+    radio.sendString("hi")
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showString("SENDHELP!")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+})
 radio.onReceivedString(function (receivedString) {
-    basic.showIcon(IconNames.Heart)
+    basic.showString("SENDHELP!")
 })
 input.onButtonPressed(Button.B, function () {
     stopwatch_start = input.runningTime()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    radio.sendValue("name", 0)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    basic.showString("HOWAREYOU?")
 })
 input.onGesture(Gesture.Shake, function () {
     if (input.logoIsPressed()) {
@@ -25,8 +42,12 @@ input.onGesture(Gesture.Shake, function () {
     }
     basic.showNumber(randint(0, 6))
 })
+radio.onReceivedValue(function (name, value) {
+    basic.showString("HOWAREYOU?")
+})
 basic.forever(function () {
     radio.setGroup(165)
+    hello = 0
     degrees = input.compassHeading()
     if (degrees < 45) {
         basic.showString("N")
